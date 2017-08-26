7947b91a99f3c10568a5e4baa2483c1cbfa5e342
Bibushka <bianca_bucur03@yahoo.com>
2017-08-26 09:35:51 +0300

    Groceries test: total test

---
+++ Groceries/Groceries/Groceries.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Groceries
+{
+
+    public struct Produse
+    {
+        public string name;
+        public double price;
+
+        public Produse(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    [TestClass]
+    public class Groceries
+    {
+        [TestMethod]
+        public void GetTotalPriceFail()
+        {
+            var groceriesCart = new Produse[] { new Produse("Bread", 4), new Produse("Wine", 56) };
+            Assert.AreEqual(0, CalculateTotal(groceriesCart));
+        }
+
+        [TestMethod]
+        public void GetTotalPrice()
+        {
+            var groceriesCart = new Produse[] { new Produse("Bread", 4), new Produse("Wine", 56) };
+            Assert.AreEqual(60, CalculateTotal(groceriesCart));
+        }
+
+        public double CalculateTotal(Produse[] groceriesCart)
+        {
+            double total = 0;
+            for (int i = 0; i < groceriesCart.Length; i++)
+                total += groceriesCart[i].price;
+            return total;
+        }
+    }
+}
