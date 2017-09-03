c36b61762a2621bd1b9ada45cb0c40eeea5a6412
Bibushka <bianca_bucur03@yahoo.com>
2017-09-04 01:03:11 +0300

    Cycling test: best average speed test

--- Cycling/Cycling/Cycling.cs
+++ Cycling/Cycling/Cycling.cs
@@ -22,8 +22,16 @@ namespace Cycling
         {
             double distance = 0;
             for (int i = 0; i < rotationsPerSecond.Length; i++)
-                distance += 3.14 * distance * rotationsPerSecond[i];
+                distance += 3.14 * diametre * rotationsPerSecond[i];
             return distance;
+        }
+
+        public double GetAverageSpeed()
+        {
+            double averageSpeed = 0;
+            for (int i = 0; i < rotationsPerSecond.Length; i++)
+                averageSpeed += rotationsPerSecond[i];
+            return averageSpeed / rotationsPerSecond.Length;
         }
     }
 
@@ -50,6 +58,16 @@ namespace Cycling
             CollectionAssert.AreEqual(new string[] { "John", "2" }, FindFastestCyclist(cyclingTeam));
         }
 
+        [TestMethod]
+        public void GetBestMedSpeed()
+        {
+            var steve = new Cyclist("steve", 1, new double[] { 23, 26, 27 });
+            var harvey = new Cyclist("Harvey", 1.2, new double[] { 25, 26, 21 });
+            var john = new Cyclist("John", 1.5, new double[] { 24, 26, 29 });
+            var cyclingTeam = new Cyclist[] { steve, harvey, john };
+            Assert.AreEqual(26.33, FindBestAverageSpeed(cyclingTeam), 2);
+        }
+
         public double CalculateDistance(Cyclist[] cyclingTeam)
         {
             double distance = 0;
@@ -74,6 +92,22 @@ namespace Cycling
             string [] fastestCyclist = new string[] { name, Convert.ToString(index)};
             return fastestCyclist;
         }
-        
+
+        public double FindBestAverageSpeed(Cyclist[] cyclingTeam)
+        {
+            double[] speeds = new double[cyclingTeam.Length];
+            for (int i = 0; i < cyclingTeam.Length; i++)
+                speeds[i] = cyclingTeam[i].GetAverageSpeed();
+            return GetMaxValue(speeds);
+        }
+
+        public double GetMaxValue(double[] speeds)
+        {
+            double maxValue = 0;
+            for (int i = 0; i < speeds.Length; i++)
+                if (speeds[i] > maxValue)
+                    maxValue = speeds[i];
+            return maxValue;
+        }
     }
 }
