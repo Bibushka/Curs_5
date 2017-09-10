48be713e5381085e1c2af838f8531a2bcb0712d0
Bibushka <bianca_bucur03@yahoo.com>
2017-09-10 19:22:44 +0300

    Points test: array version

--- Points/Points/Points.cs
+++ Points/Points/Points.cs
@@ -6,53 +6,37 @@ namespace Points
 
     public struct Point
     {
-        public int x;
-        public int y;
-
-        public Point(int x, int y)
+        public int[,] trail;
+        public int[,] trailToCheck;
+        public Point(int[,] trail)
         {
-            this.x = x;
-            this.y = y;
-        }
-
-        public void MovePoint(int x, int y)
-        {
-            this.x += x;
-            this.y += y;
+            this.trail = trail;
+            this.trailToCheck = new int[trail.Length, 2];
+            for (int i = 0; i < trail.Length; i++)
+            {
+                trailToCheck[i, 0] += trail[i, 0];
+                trailToCheck[i, 1] += trail[i, 1];
+            }
         }
     }
-
+    
     [TestClass]
     public class Points
     {
         [TestMethod]
         public void TestMethod1()
         {
-            var initialPoint = new Point (0, 0);
-            int[,] trail = new int[,] { { -1, 1 }, { 1, 1 } };
-            Assert.IsTrue(CheckIntersection(initialPoint, trail));
+            var initialPoint = new Point (new int[,] { { -1, 1 }, { 1, 1 } });
+            Assert.IsTrue(CheckIntersection(initialPoint));
         }
 
-        public bool CheckIntersection(Point initialPoint, int[,] trail)
-        {
-            int[,] trailToCheck = new int[trail.Length+1, 2];
-            trailToCheck[0,0] = initialPoint.x;
-            trailToCheck[0,1] = initialPoint.y;
-            for (int i = 0; i < trail.Length; i++)
-            {
-                initialPoint.MovePoint(trail[i, 0], trail[i, 1]);
-                trailToCheck[i, 0] = initialPoint.x;
-                trailToCheck[i, 1] = initialPoint.y;
-            }
-            return CheckTrail(trailToCheck);
-        }
-
-        public bool CheckTrail(int[,] trailToCheck)
+        public bool CheckIntersection(Point initialPoint)
         {
             int ok = 0;
-            for (int i = 0; i < trailToCheck.Length - 1; i++)
-                for (int j = 1; j < trailToCheck.Length; i++)
-                    if (trailToCheck[i, 0] == trailToCheck[j, 0] && trailToCheck[i, 1] == trailToCheck[j, 1])
+            for (int i = 0; i < initialPoint.trailToCheck.Length - 1; i++)
+                for (int j = 1; j < initialPoint.trailToCheck.Length; i++)
+                    if (initialPoint.trailToCheck[i, 0] == initialPoint.trailToCheck[j, 0] && 
+                        initialPoint.trailToCheck[i, 1] == initialPoint.trailToCheck[j, 1])
                         ok = 1;
             return (ok == 1);
         }
