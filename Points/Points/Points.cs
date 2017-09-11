c6359638774c6f8132afe963ae1e4ec032aa79e8
Bibushka <bianca_bucur03@yahoo.com>
2017-09-11 23:19:39 +0300

    Points test:

--- Points/Points/Points.cs
+++ Points/Points/Points.cs
@@ -15,13 +15,13 @@ namespace Points
             this.x = x;
             this.y = y;
             this.trail = trail;
-            this.trailToCheck = new int[trail.Length+1, 2];
+            this.trailToCheck = new int[trail.Length/2+1, 2];
             trailToCheck[0, 0] = x;
             trailToCheck[0, 1] = y;
-            for (int i = 0; i < trail.Length; i++)
+            for (int i = 0; i < trail.Length/2; i++)
             {
-                trailToCheck[i+1, 0] += trail[i, 0];
-                trailToCheck[i+1, 1] += trail[i, 1];
+                trailToCheck[i+1, 0] = trailToCheck[i, 0] + trail[i, 0];
+                trailToCheck[i+1, 1] = trailToCheck[i, 1] + trail[i, 1];
             }
         }
     }
@@ -30,21 +30,27 @@ namespace Points
     public class Points
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckIfThereIsAnInterseciom()
         {
-            var initialPoint = new Point (0, 0, new int[,] { { -1, 1 }, { 1, 1 } });
-            Assert.IsTrue(CheckIntersection(initialPoint));
+            var initialPoint = new Point (0, 0, new int[,] { { -1, 1 }, { 1, -1 } });
+            Assert.IsTrue(CheckIntersection(initialPoint.trailToCheck));
         }
 
-        public bool CheckIntersection(Point initialPoint)
+        [TestMethod]
+        public void CheckIfThereIsNotAnInterseciom()
         {
-            int ok = 0;
-            for (int i = 0; i < initialPoint.trailToCheck.Length - 1; i++)
-                for (int j = 1; j < initialPoint.trailToCheck.Length; i++)
-                    if (initialPoint.trailToCheck[i, 0] == initialPoint.trailToCheck[j, 0] && 
-                        initialPoint.trailToCheck[i, 1] == initialPoint.trailToCheck[j, 1])
-                        ok = 1;
-            return (ok == 1);
+            var initialPoint = new Point(0, 0, new int[,] { { -1, 1 }, { 1, 1 } });
+            Assert.IsFalse(CheckIntersection(initialPoint.trailToCheck));
+        }
+
+        public bool CheckIntersection(int[,] trailToCheck)
+        {
+            for (int i = 0; i < trailToCheck.Length / 2 - 1; i++)
+                for (int j = 1; j < trailToCheck.Length / 2; i++)
+                    if (trailToCheck[i, 0] == trailToCheck[j, 0] &&
+                        trailToCheck[i, 1] == trailToCheck[j, 1])
+                        return true;
+            return false;
         }
     }
 }
