51ae0c830c2b76607b2a8aa5cd6d7afe77063fd8
Bibushka <bianca_bucur03@yahoo.com>
2017-09-23 16:51:00 +0300

    Password test: final

--- Password/Password/Password.cs
+++ Password/Password/Password.cs
@@ -9,6 +9,7 @@ namespace Password
         public int numberOfCapitalLetters;
         public int numberOfDigits;
         public int numberOfCharacters;
+        public string yourPassword;
 
         public Password(int numberOfSmallLetters, int numberOfCapitalLetters,
             int numberOfDigits, int numberOfCharacters)
@@ -17,13 +18,13 @@ namespace Password
             this.numberOfCapitalLetters = numberOfCapitalLetters;
             this.numberOfDigits = numberOfDigits;
             this.numberOfCharacters = numberOfCharacters;
+            yourPassword = string.Empty;
         }
 
-        public string GeneratePassword(string smallLetters, string capitalLetters,
+        public void GeneratePassword(string smallLetters, string capitalLetters,
             string digits, string characters)
         {
-            string yourPassword = string.Empty;
-            return yourPassword = smallLetters + capitalLetters + digits + characters;
+            yourPassword = smallLetters + capitalLetters + digits + characters;
         }
     }
 
@@ -33,20 +34,103 @@ namespace Password
         [TestMethod]
         public void IsPasswordCorrectlyComposed()
         {
-            var yourPassword = new Password(2, 3, 4, 1);
-            Assert.IsTrue(ValidatePassword(yourPassword));
+            var passwordToCheck = new Password(2, 3, 4, 1);
+            string lowerCaseLetters = GenerateSmallLetters(passwordToCheck.numberOfSmallLetters);
+            string upperCaseLetters = GenerateCapitalLetters(passwordToCheck.numberOfCapitalLetters);
+            string digits = GenerateDigits(passwordToCheck.numberOfDigits);
+            string characters = GenerateCharacters(passwordToCheck.numberOfCharacters);
+            passwordToCheck.GeneratePassword(lowerCaseLetters,upperCaseLetters, digits, characters);
+            Assert.IsTrue(ValidatePassword(passwordToCheck));
         }
 
-        public bool ValidatePassword(Password yourPassword)
+        [TestMethod]
+        public void IsPasswordInorrectlyComposed()
         {
-            string lowerCaseLetters = GenerateSmallLetters(yourPassword.numberOfSmallLetters);
-            string upperCaseLetters = GenerateCapitalLetters(yourPassword.numberOfCapitalLetters);
-            string digits = GenerateDigits(yourPassword.numberOfDigits);
-            string characters = GenerateCharacters(yourPassword.numberOfCharacters);
-            string passwordToCheck = yourPassword.GeneratePassword(lowerCaseLetters, 
-                upperCaseLetters, digits, characters);
-            return true;
+            var passwordToCheck = new Password(2, 3, 4, 1);
+            string lowerCaseLetters = GenerateSmallLetters(passwordToCheck.numberOfSmallLetters);
+            string upperCaseLetters = GenerateCapitalLetters(passwordToCheck.numberOfCapitalLetters);
+            string digits = GenerateDigits(passwordToCheck.numberOfDigits);
+            string characters = GenerateCharacters(passwordToCheck.numberOfCharacters);
+            passwordToCheck.GeneratePassword(lowerCaseLetters, upperCaseLetters, digits, characters);
+            passwordToCheck.yourPassword = RuinPassword(passwordToCheck.yourPassword, '0');
+            Assert.IsFalse(ValidatePassword(passwordToCheck));
         }
+
+        [TestMethod]
+        public void CheckForSimilarCharactersTrue()
+        {
+            var passwordToCheck = new Password(2, 3, 4, 1);
+            string lowerCaseLetters = GenerateSmallLetters(passwordToCheck.numberOfSmallLetters);
+            string upperCaseLetters = GenerateCapitalLetters(passwordToCheck.numberOfCapitalLetters);
+            string digits = GenerateDigits(passwordToCheck.numberOfDigits);
+            string characters = GenerateCharacters(passwordToCheck.numberOfCharacters);
+            passwordToCheck.GeneratePassword(lowerCaseLetters, upperCaseLetters, digits, characters);
+            Assert.IsTrue(ValidatePassword(passwordToCheck));
+        }
+
+        [TestMethod]
+        public void CheckForSimilarCharactersFalse()
+        {
+            var passwordToCheck = new Password(2, 3, 4, 1);
+            string lowerCaseLetters = GenerateSmallLetters(passwordToCheck.numberOfSmallLetters);
+            string upperCaseLetters = GenerateCapitalLetters(passwordToCheck.numberOfCapitalLetters);
+            string digits = GenerateDigits(passwordToCheck.numberOfDigits);
+            string characters = GenerateCharacters(passwordToCheck.numberOfCharacters);
+            passwordToCheck.GeneratePassword(lowerCaseLetters, upperCaseLetters, digits, characters);
+            passwordToCheck.yourPassword = RuinPassword(passwordToCheck.yourPassword, '1');
+            Assert.IsFalse(ValidatePassword(passwordToCheck));
+        }
+
+        [TestMethod]
+        public void CheckForAmbiguousCharactersTrue()
+        {
+            var passwordToCheck = new Password(2, 3, 4, 1);
+            string lowerCaseLetters = GenerateSmallLetters(passwordToCheck.numberOfSmallLetters);
+            string upperCaseLetters = GenerateCapitalLetters(passwordToCheck.numberOfCapitalLetters);
+            string digits = GenerateDigits(passwordToCheck.numberOfDigits);
+            string characters = GenerateCharacters(passwordToCheck.numberOfCharacters);
+            passwordToCheck.GeneratePassword(lowerCaseLetters, upperCaseLetters, digits, characters);
+            Assert.IsTrue(ValidatePassword(passwordToCheck));
+        }
+
+        [TestMethod]
+        public void CheckForAmbiguousCharactersFalse()
+        {
+            var passwordToCheck = new Password(2, 3, 4, 1);
+            string lowerCaseLetters = GenerateSmallLetters(passwordToCheck.numberOfSmallLetters);
+            string upperCaseLetters = GenerateCapitalLetters(passwordToCheck.numberOfCapitalLetters);
+            string digits = GenerateDigits(passwordToCheck.numberOfDigits);
+            string characters = GenerateCharacters(passwordToCheck.numberOfCharacters);
+            passwordToCheck.GeneratePassword(lowerCaseLetters, upperCaseLetters, digits, characters);
+            passwordToCheck.yourPassword = RuinPassword(passwordToCheck.yourPassword, '>');
+            Assert.IsFalse(ValidatePassword(passwordToCheck));
+        }
+
+        public bool ValidatePassword(Password passwordToCheck)
+        {
+            int countOfLowerCaseLetters = 0;
+            int countOfUpperCaseLetters = 0;
+            int countOfDigits = 0;
+            int countOfCharacters = 0;
+            for (int i = 0; i < passwordToCheck.yourPassword.Length; i++)
+            {
+                if ("abcdefghijkmnpqrstuvwxyz".Contains(passwordToCheck.yourPassword[i].ToString()))
+                    countOfLowerCaseLetters++;
+                if ("ABCDEFGHJKLMNPQRSTUVWXYZ".Contains(passwordToCheck.yourPassword[i].ToString()))
+                    countOfUpperCaseLetters++;
+                if ("23456789".Contains(passwordToCheck.yourPassword[i].ToString()))
+                    countOfDigits++;
+                if (!"{ }[]()/\"~,;.<>0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".Contains(passwordToCheck.yourPassword[i].ToString()))
+                    countOfCharacters++;
+            }
+            if (countOfLowerCaseLetters == passwordToCheck.numberOfSmallLetters &&
+                countOfUpperCaseLetters == passwordToCheck.numberOfCapitalLetters &&
+                countOfDigits == passwordToCheck.numberOfDigits &&
+                countOfCharacters == passwordToCheck.numberOfCharacters)
+                return true;
+            return false;
+        }
+
 
         public string GenerateSmallLetters(int numberOfLetters)
         {
@@ -122,5 +206,11 @@ namespace Password
             return (char)(random.Next(lowerChar, upperChar+1));
         }
 
+        public string RuinPassword(string yourPassword, char badCharacter)
+        {
+            char[] passwordToRuing = yourPassword.ToCharArray();
+            passwordToRuing[5] = badCharacter;
+            return yourPassword = new string(passwordToRuing);
+        }
     }
 }
